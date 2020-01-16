060f4da497115203ae408ed91f0c76bc3ed6cdb4
Matheus Soares <mathsoares1@gmail.com>
2020-01-16 18:26:09 -0400

    Adicionado jogador com acao de saltar

--- game.js
+++ game.js
@@ -8,6 +8,24 @@ var gap = randomGap();
 
 var myObstacles = [];
 
+var player = {
+  x: 20,
+  y: 470,
+  speedY: 0,
+  update: function() {
+    gameArea.context.fillRect(this.x, this.y, 30, 30);
+  },
+  newPos: function() {
+    if (this.y < 280)
+      this.speedY = 2;
+    
+    this.y = this.y + this.speedY;
+
+    if (this.speedY === 2 && this.y === 470)
+      this.speedY = 0;
+  }
+};
+
 var gameArea = {
   canvas: document.createElement("canvas"),
   start: function() {
@@ -17,6 +35,7 @@ var gameArea = {
     this.context = this.canvas.getContext("2d");
     this.frame = 0;
     this.interval = setInterval(this.updateGameArea, 5);
+    window.addEventListener("keydown", jump);
   },
   updateGameArea: function() {
     gameArea.clear()
@@ -31,13 +50,15 @@ var gameArea = {
       item.x -= 1;
       item.draw();
     });
+    player.newPos();
+    player.update();
     gameArea.frame += 1
   },
   clear: function() {
     gameArea.context.clearRect(0,0,this.canvas.width, this.canvas.height);
   },
   stop: function() {
-
+    player.newPos();
   }
 };
 
@@ -62,6 +83,10 @@ function everyInterval(n) {
 
 function randomGap() {
   return Math.floor(minGap + Math.random() * (maxGap - minGap + 1));
+}
+
+function jump() {
+  player.speedY = -2;
 }
 
 startGame();
